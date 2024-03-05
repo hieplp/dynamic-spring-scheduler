630161626f06ae8a45bec7f3170ecf2483e96523
Hiệp Lý <hiepphuocly@gmail.com>
2024-03-05 22:57:57 +0700

    feat: add create page ui

--- src/main/java/dev/hieplp/spring/scheduler/controller/IndexController.java
+++ src/main/java/dev/hieplp/spring/scheduler/controller/IndexController.java
@@ -1,13 +1,12 @@
 package dev.hieplp.spring.scheduler.controller;
 
-import dev.hieplp.spring.scheduler.common.model.JobModel;
+import dev.hieplp.spring.scheduler.common.payload.request.job.InteractJobRequest;
 import dev.hieplp.spring.scheduler.service.JobService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,17 +22,24 @@ public class IndexController {
     public String index(Model map) {
         log.info("Index page");
 
-        var jobs = jobService.getAll();
+        final var jobs = jobService.getAll();
         map.addAttribute("jobs", jobs);
-
-        map.addAttribute("job", new JobModel());
 
         return "index";
     }
 
+    @GetMapping("/create")
+    public String create(Model map) {
+        log.info("Create page");
+        return "create";
+    }
+
     @PostMapping("/interact")
-    public String interact(@ModelAttribute("job") JobModel job) {
-        log.info("Interact job: {}", job);
+    public String interact(InteractJobRequest request) {
+        log.info("Interact with job: {}", request);
+
+        jobService.interact(request);
+
         return "redirect:/";
     }
 }
